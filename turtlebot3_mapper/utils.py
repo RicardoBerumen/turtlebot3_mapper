2f53a5a2fbfd3900a752aa517c0d453076e12caa
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2022-12-17 22:49:33 -0300

    :page_facing_up: Add license, contributing guidelines and copyrights.
    * Add Apache 2.0 License;
    * Initial contributing guidelines;
    * Add in each source file a license and copyright statement.
    
    Signed-off-by: Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>

--- turtlebot3_mapper/utils.py
+++ turtlebot3_mapper/utils.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Luiz Carlos Cosmi Filho and others.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Tuple
 
 import numpy as np
@@ -11,11 +25,11 @@ from geometry_msgs.msg import Quaternion
 
 def euler_from_quaternion(quaternion: Quaternion):
     """ Convert quaternion (w in last place) to euler roll, pitch, yaw.
-    
+
     Parameters
     ----------
     quaternion: :py:class:`Quarternion <geometry_msgs.msg._quaternion.Quaternion>`
-        Orientation as a quaternion.         
+        Orientation as a quaternion.
 
     Returns
     -------
